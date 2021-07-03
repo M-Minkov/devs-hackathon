b1703c97a39d90b48e714040562559c96884cc92
M-Minkov <MinkoMinkov@outlook.co.nz>
2021-07-03 17:20:32 +1200

    here we go

--- front-end/algobattle/src/components/game/game.js
+++ front-end/algobattle/src/components/game/game.js
@@ -1,27 +1,35 @@
+import React, { useEffect, useState } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import Ide from "./ide";
 import Prompt from "./prompt";
 import classes from "./game.module.css";
+import axios from 'axios';
 
 function Game() {
+  const [problemInfoJSON, setProblemInfoJSON] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/problem_info').then( (problemInformation) => {
+      setProblemInfoJSON(problemInformation.data);
+    })
+
+  }, [])
+
+  function return_text(probleminfo) {
+    return (probleminfo.here)
+  }
+
+  console.log(problemInfoJSON);
+
+
   return (
     <div className={classes.background}>
       <Grid container spacing={2}>
         <Grid item xs>
-          <Prompt
-            title="Title"
-            description="Sed ut perspiciatis unde omnis iste natus error sit 
-            voluptatem accusantium doloremque laudantium, totam rem aperiam, 
-            eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae 
-            vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit 
-            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui 
-            ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem 
-            ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam 
-            eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
-            Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam,
-             nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate 
-             velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-          />
+          {<Prompt
+            title={return_text(problemInfoJSON)}
+            description={return_text(problemInfoJSON)}
+            />}
         </Grid>
         <Grid item xs>
           <Ide />
